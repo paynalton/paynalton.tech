27e69e11db28d4c2d4f8124e7f28281ff67044d0
Paynalton <cxescalona@gmail.com>
2025-04-17 16:30:48 -0600

    nuevo sitio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Paynalton | Experto en Desarrollo de Software y Solución de Problemas';
+export const SITE_DESCRIPTION = 'Experiencia que transforma problemas en progreso';
