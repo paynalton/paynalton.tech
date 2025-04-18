9753cd134ad321cc0eca987eacb79254484f7234
Paynalton <cxescalona@gmail.com>
2025-04-18 11:32:13 -0600

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://paynalton.tech',
+    integrations: [mdx(), sitemap(), vue()],
 });
